2195e37fe196267632bbe63b93cb6d73f080ca41
vscabell <vscabell@student.42sp.org.br>
2021-02-24 02:22:17 +0100

    add ft_strcmp into libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: vscabell <vscabell@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 11:11:03 by vscabell          #+#    #+#             */
-/*   Updated: 2021/02/17 23:00:14 by vscabell         ###   ########.fr       */
+/*   Updated: 2021/02/23 00:54:05 by vscabell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,5 +78,6 @@ char				*ft_itoa_base(int n, char *base, int size_base);
 void				ft_putnbr_base_fd(unsigned int n, char *base, int fd);
 void				ft_swap(int *a, int *b);
 int					get_next_line(int fd, char **line);
+int					ft_strcmp(const char *s1, const char *s2);
 
 #endif
